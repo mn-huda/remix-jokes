e8b973156f05bc3824ba09d6952eed876a4b2d2a
Md. Nazmul Huda <me@nazmul-huda.com>
2022-07-12 13:57:56 +0600

    first commit

---
+++ app/routes/jokes/$jokeId.jsx
@@ -0,0 +1,24 @@
+import { json } from "@remix-run/node"
+import { Link, useLoaderData } from "@remix-run/react"
+
+import { db } from "~/utils/db.server"
+
+export const loader = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId }
+  })
+  if (!joke) throw new Error("Joke not found")
+  const data = { joke }
+  return json(data)
+}
+
+export default function JokeRoute() {
+  const data = useLoaderData()
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
+    </div>
+  )
+}
---
+++ app/routes/jokes/index.jsx
@@ -0,0 +1,28 @@
+import { json } from "@remix-run/node"
+import { useLoaderData, Link } from "@remix-run/react"
+
+import { db } from "~/utils/db.server"
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber
+  })
+  const data = { randomJoke };
+  return json(data);
+}
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData()
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>
+        "{data.randomJoke.name}" Permalink
+      </Link>
+    </div>
+  )
+}
